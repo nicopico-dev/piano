57a48fec2f1ce8c1f6b1fb5e5d34a0c0ba13b4bb
Nicolas Picon <nicopico.dev@gmail.com>
2018-12-04 23:39:09 +0100

    Simplify flutter_midi API

--- flutter_midi/lib/flutter_midi.dart
+++ flutter_midi/lib/flutter_midi.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
+import 'package:meta/meta.dart';
 
 class FlutterMidi {
   static const MethodChannel _channel = MethodChannel('flutter_midi');
@@ -14,10 +14,14 @@ class FlutterMidi {
     return result;
   }
 
-  /// Unmute the device temporarly even if the mute switch is on or toggled in settings.
-  static Future<String> unmute() async {
-    final String result = await _channel.invokeMethod('unmute');
-    return result;
+  /// Play a midi note from the sound_font.SF2 library bundled with the application.
+  /// Play a midi note in the range between 0-256
+  /// Multiple notes can be played at once as seperate calls.
+  static Future<String> playMidiNote({@required int midi}) async {
+    return await _channel.invokeMethod(
+      'play_midi_note',
+      {'note': midi},
+    );
   }
 
   /// Use this when stopping the sound onTouchUp or to cancel a long file.
@@ -25,24 +29,5 @@ class FlutterMidi {
   static Future<String> stopMidiNote() async {
     final String result = await _channel.invokeMethod('stop_midi_note');
     return result;
-  }
-
-  /// Play a midi note from the sound_font.SF2 library bundled with the application.
-  /// Play a midi note in the range between 0-256
-  /// Multiple notes can be played at once as seperate calls.
-  static Future<String> playMidiNote({
-    @required int midi,
-
-    /// Force play the sound even if the mute switch is on or toggled on in the settings.
-    /// This will not always work on every device, but is required this way by Apple.
-    bool unmute,
-  }) async {
-    final Map<dynamic, dynamic> mapData = <dynamic, dynamic>{};
-    print("Pressed: $midi");
-    mapData["note"] = midi;
-    if (unmute != null && unmute) {
-      return await _channel.invokeMethod('play_midi_note_unmute', mapData);
-    }
-    return await _channel.invokeMethod('play_midi_note', mapData);
   }
 }
